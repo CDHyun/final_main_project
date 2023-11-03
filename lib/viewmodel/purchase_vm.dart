84a445e0ac40839ab5e72aa1df181fbdbf9dc9c2
kangdeagyu <rkdeorb0124@naver.com>
2023-11-03 17:00:14 +0900

    광고 코인 수정

--- lib/viewmodel/purchase_vm.dart
+++ lib/viewmodel/purchase_vm.dart
@@ -140,7 +140,7 @@ class PurchaseVM extends GetxController {
       {
         'category': "ads", // 카드번호
         'price': coin, // 충전이든 사용이든 여기에 넣어주기.
-        'coinHistory': myCoin.value + 10,
+        'coinHistory': myCoin.value + 50,
         'usedate': Timestamp.fromDate(DateTime.now())
       },
     );
